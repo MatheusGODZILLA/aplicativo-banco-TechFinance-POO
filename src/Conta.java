92c718700a919927e65bfe05869df553f49e1be4
MatheusGODZILLA <contagodzillajapan@gmail.com>
2023-09-12 01:57:29 -0300

    Definindo atributos, construtor da classe e lista

--- src/Conta.java
+++ src/Conta.java
@@ -3,5 +3,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Conta {
-    
+    double saldo;
+    int numeroConta;
+    Cliente cliente;
+
+    private List<String> historicoTransacoes; // Lista para armazenar o histórico de transações
+
+    // Construtor da classe
+    public Conta(double saldo, int numeroConta, Cliente cliente) {
+        this.saldo = saldo;
+        this.numeroConta = numeroConta;
+        this.cliente = cliente;
+        this.historicoTransacoes = new ArrayList<>();
+    }
+
+    public Conta(double saldoInicial, String numeroConta2, Cliente cliente2) {
+    }
 }
