1a877317396e85c73caec1951f289217bf8a1b2c
MatheusGODZILLA <contagodzillajapan@gmail.com>
2023-09-12 02:41:12 -0300

    Criando a lista e os métodos da classe

--- src/Notificação.java
+++ src/Notificação.java
@@ -3,5 +3,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Notificação {
-    
+    private List<String> mensagens;
+
+    public Notificação() {
+        this.mensagens = new ArrayList<>();
+    }
+
+    // Método para adicionar uma mensagem de notificação
+    public void adicionarMensagem(String mensagem) {
+        mensagens.add(mensagem);
+    }
+
+    // Método para obter todas as mensagens de notificação
+    public List<String> getMensagens() {
+        return mensagens;
+    }
+
+    // Método para limpar todas as mensagens de notificação
+    public void limparMensagens() {
+        mensagens.clear();
+    }
 }
