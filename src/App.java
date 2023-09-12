44addef3c7f2d87f8e8d57a0bb082adaa6b8c109
MatheusGODZILLA <contagodzillajapan@gmail.com>
2023-09-12 02:03:06 -0300

    Cadastro inicial do cliente

--- src/App.java
+++ src/App.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in); // Cria um objeto Scanner para leitura de entrada
+
+        // Cadastro inicial do cliente
+        System.out.println("//-- Cadastro inicial --//");
+        System.out.print("Nome do Cliente: ");
+        String nome = scanner.nextLine();
+        System.out.print("CPF: ");
+        String CPF = scanner.nextLine();
+        System.out.print("Data de Nascimento: ");
+        String dataNascimento = scanner.nextLine();
+        System.out.print("Saldo Inicial: ");
+        double saldoInicial = scanner.nextDouble();
+        scanner.nextLine(); // Limpar a linha em branco
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
     }
 }
